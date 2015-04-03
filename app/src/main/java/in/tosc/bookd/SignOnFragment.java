bebb815b117c8fd7862c8ba221e5d15c52bab756
Prempal Singh <prempal.42@gmail.com>
2015-04-03 22:49:34 +0530

    signon fragment setup

---
+++ app/src/main/java/in/tosc/bookd/SignOnFragment.java
@@ -0,0 +1,25 @@
+package in.tosc.bookd;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by prempal on 3/4/15.
+ */
+public class SignOnFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_signon, container, false);
+        return rootView;
+    }
+}
