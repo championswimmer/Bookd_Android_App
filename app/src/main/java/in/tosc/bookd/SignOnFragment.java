6c23776a303fa0ffefeee7444a3e9bdbea978a0b
Prempal Singh <prempal.42@gmail.com>
2015-04-03 23:51:33 +0530

    sign up functions

--- app/src/main/java/in/tosc/bookd/SignOnFragment.java
+++ app/src/main/java/in/tosc/bookd/SignOnFragment.java
@@ -6,11 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by prempal on 3/4/15.
  */
-public class SignOnFragment extends Fragment {
+public class SignOnFragment extends Fragment implements View.OnClickListener{
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,52 @@ public class SignOnFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_signon, container, false);
+        EditText username = (EditText) rootView.findViewById(R.id.et_username);
+        EditText password = (EditText) rootView.findViewById(R.id.et_password);
+        Button signup = (Button) rootView.findViewById(R.id.btn_signup);
+        Button signin = (Button) rootView.findViewById(R.id.btn_signin);
+        Button fb_login = (Button) rootView.findViewById(R.id.btn_facebook);
+        Button twitter_login = (Button) rootView.findViewById(R.id.btn_twitter);
+
+        signup.setOnClickListener(this);
+        signin.setOnClickListener(this);
+        fb_login.setOnClickListener(this);
+        twitter_login.setOnClickListener(this);
+
         return rootView;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_signup:
+                signUp();
+                break;
+            case R.id.btn_signin:
+                signIn();
+                break;
+            case R.id.btn_facebook:
+                signInFB();
+                break;
+            case R.id.btn_twitter:
+                signInTwitter();
+                break;
+        }
+    }
+
+    private void signInTwitter() {
+
+    }
+
+    private void signInFB() {
+
+    }
+
+    private void signIn() {
+
+    }
+
+    private void signUp() {
+
     }
 }
