4b28fdd5816b4567b88ca7cca5eb8fae517863db
Prempal Singh <prempal.42@gmail.com>
2015-04-04 05:46:13 +0530

    scanner activity

---
+++ app/src/main/java/in/tosc/bookd/ScannerActivity.java
@@ -0,0 +1,47 @@
+package in.tosc.bookd;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Arrays;
+
+import me.dm7.barcodescanner.zbar.BarcodeFormat;
+import me.dm7.barcodescanner.zbar.Result;
+import me.dm7.barcodescanner.zbar.ZBarScannerView;
+
+
+/**
+ * Created by prempal on 4/4/15.
+ */
+public class ScannerActivity extends Activity implements ZBarScannerView.ResultHandler {
+    private ZBarScannerView mScannerView;
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        mScannerView = new ZBarScannerView(this);    // Programmatically initialize the scanner view
+        mScannerView.setFormats(Arrays.asList(BarcodeFormat.ISBN10,BarcodeFormat.ISBN13));
+        mScannerView.setAutoFocus(true);
+        setContentView(mScannerView);                // Set the scanner view as the content view
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();          // Start camera on resume
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+        Log.v("Result : ", rawResult.getContents()); // Prints scan results
+    }
+}
