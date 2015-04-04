2dbadf0527df285ea6719d6f2fd009fac0aa1bc3
Arnav Gupta <championswimmer@gmail.com>
2015-04-04 16:08:51 +0530

    adding books to wishlist

--- app/src/main/java/in/tosc/bookd/utilactivities/AddBookWishlistActivity.java
+++ app/src/main/java/in/tosc/bookd/utilactivities/AddBookWishlistActivity.java
@@ -1,19 +1,68 @@
 package in.tosc.bookd.utilactivities;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.concurrent.ExecutionException;
 
 import in.tosc.bookd.R;
+import in.tosc.bookd.bookapi.BookObject;
+import in.tosc.bookd.bookapi.GetBookInfo;
 
 public class AddBookWishlistActivity extends ActionBarActivity {
+
+    public static final int ADD_BOOK_WISHLIST_RESULT = 11;
+
+    BookObject bookObject;
+
+    ImageView imageBook;
+    TextView tvBookTitle, tvBookAuthor, tvBookPublisher, tvBookSummary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_book_wishlist);
+
+        imageBook = (ImageView) findViewById(R.id.image_book);
+        tvBookTitle = (TextView) findViewById(R.id.tv_book_title);
+        tvBookAuthor = (TextView) findViewById(R.id.tv_book_author);
+        tvBookPublisher = (TextView) findViewById(R.id.tv_book_publisher);
+        tvBookSummary = (TextView) findViewById(R.id.tv_book_summary);
+
+        Intent scan = new Intent(this, ScannerActivity.class);
+        startActivityForResult(scan, ADD_BOOK_WISHLIST_RESULT);
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == ADD_BOOK_WISHLIST_RESULT) {
+
+            GetBookInfo gbi = new GetBookInfo(getApplicationContext());
+            try {
+                bookObject = gbi.execute(data.getStringExtra("ISBN")).get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+
+            if (bookObject != null) {
+                tvBookTitle.setText(bookObject.getTitle());
+                tvBookAuthor.setText(bookObject.getAuthor());
+                tvBookPublisher.setText(bookObject.getPublisher());
+                tvBookSummary.setText(bookObject.getSummary());
+                //TODO: Also need to set the book image
+            }
+            //TODO: What if we do not get a result ?? Do something about that too
+
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
